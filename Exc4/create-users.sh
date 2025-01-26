6e9bffa1e632cc2245d9f2634949671b97332f7f
Соловьев Сергей Николаевич <solovyov_sn@tochka.com>
2025-01-26 20:59:56 +0500

    HW

---
+++ Exc4/create-users.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl create serviceaccount developer1 -n development
+kubectl create serviceaccount security-specialist1 -n development
+kubectl create serviceaccount devops-engineer1 -n development
+kubectl create serviceaccount ops-engineer1 -n development
+kubectl create serviceaccount product-owner1 -n development
