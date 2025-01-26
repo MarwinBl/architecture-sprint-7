70ffe410b9edda2a4803ab1de280525cc3ef5652
Соловьев Сергей Николаевич <solovyov_sn@tochka.com>
2025-01-26 21:20:31 +0500

    HW

--- Exc4/create-roles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f all-viewer.yaml
-kubectl apply -f cluster-admin.yaml
-kubectl apply -f secret-viewer.yaml
--- Exc4/create-users.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create serviceaccount developer1 -n development
-kubectl create serviceaccount security-specialist1 -n development
-kubectl create serviceaccount devops-engineer1 -n development
-kubectl create serviceaccount ops-engineer1 -n development
-kubectl create serviceaccount product-owner1 -n development
